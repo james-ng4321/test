fb4a71cd380b590ca703fd56de02aef0d3e4d0a0
James Ng <james.ng4321@gmail.com>
2025-07-27 01:00:55 +0800

    1

--- ACmi5_2.js
+++ ACmi5_2.js
@@ -49,7 +49,7 @@ let hour = now.getHours();
 let min = now.getMinutes();
 let sec = now.getSeconds();
 let msUntilNextHour = ((60 - min - 1) * 60 * 1000) + (60 - sec) * 1000;
-let ms = ["3001","3002","3003","3001","3001","3002","3003","3005"];
+let ms = ["3002","3003","3004","3005","3006","3007","3003","3005"];
 let ms1=Math.floor(Math.random() * 8);
 let msUntilNextHour2= msUntilNextHour  - ms[ms1];
 
