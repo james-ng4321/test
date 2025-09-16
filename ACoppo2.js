77bac68cc1eae8b993b767296ed7cbe22ec7014a
James Ng <james.ng4321@gmail.com>
2025-09-17 01:38:11 +0800

    1

--- ACoppo2.js
+++ ACoppo2.js
@@ -47,7 +47,8 @@ let min = now.getMinutes();
 let sec = now.getSeconds();
 let msUntilNextHour = ((60 - min - 1) * 60 * 1000) + (60 - sec) * 1000;
 //let ms = ["4100","4200","4300","4400","4150","4250","4350","4450"];
-let ms = ["4100","4200","4300","4400","4150","4250","4350","4450"];
+//let ms = ["4100","4200","4300","4400","4150","4250","4350","4450"];
+let ms = ["5100","5200","5300","5400","5150","5250","5350","5450"];
 let ms1=Math.floor(Math.random() * 8);
 let msUntilNextHour2= msUntilNextHour - ms[ms1];
 setTimeout(function() {
