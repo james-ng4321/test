5635748a613807eac8264c78d1928f595c995026
James Ng <james.ng4321@gmail.com>
2025-09-28 01:14:06 +0800

    1

--- ACmi5_6 - Copy.js
+++ ACmi5_6 - Copy.js
@@ -1,11 +1,12 @@
-var a =http.get('https://raw.githubusercontent.com/james-ng4321/test/refs/heads/master/mi5_5');
+var a =http.get('https://raw.githubusercontent.com/james-ng4321/test/refs/heads/master/mi5_6');
 var b=a.body.string();
 var c=b.split(',');
 
-//home();
-//sleep(1000);
-///launchApp('WireGuard');
-//sleep(6000);
+home();
+sleep(1000);
+launchApp('WireGuard');
+//className("android.widget.TextView").text("WireGuard").waitFor()
+sleep(6000);
 
 home();
 sleep(1000); 
@@ -50,16 +51,13 @@ let min = now.getMinutes();
 let sec = now.getSeconds();
 let msUntilNextHour = ((60 - min - 1) * 60 * 1000) + (60 - sec) * 1000;
 let ms = ["3700","3800","3900","3950","3750","3850","3900","3950"];
-//let ms = ["4100","4200","4300","4400","4150","4250","4350","4450"];
-//let ms = ["3010","3020","3030","3010","3010","3020","3030","3010"];
 let ms1=Math.floor(Math.random() * 8);
 let msUntilNextHour2= msUntilNextHour  - ms[ms1];
 
 setTimeout(function() {
     click(63,627);
-    sleep(65000);
-    click(63,627);
-//   book2();
+    sleep(55000);
+    book2();
 }, msUntilNextHour2);
 
 
@@ -72,16 +70,17 @@ function book2(){
   // className("android.widget.Button").desc("虛擬等候室").findOne();
   // sleep(1000);
   //  click(63,627);
+    click(63,627);
     sleep(1000);
      className("android.view.View").desc("你的日程表").findOne();
      sleep(1000);
      click(372,1807);
      className("android.view.View").desc("最遠可預約的日期").findOne();
-     sleep(1800);
+     sleep(1300);
          swipe(778,1685,778,1276,1000);
          sleep(500);
          click(778,1452);
-         sleep(1500);
+         sleep(1100);
              if(!(className("android.view.View").desc("檢視並付款").exists())){
                click(702,1163); //  click(530,1165);
              sleep(200);
--- ACmi5_6.js
+++ ACmi5_6.js
@@ -2,11 +2,10 @@ var a =http.get('https://raw.githubusercontent.com/james-ng4321/test/refs/heads/
 var b=a.body.string();
 var c=b.split(',');
 
-home();
-sleep(1000);
-launchApp('WireGuard');
-//className("android.widget.TextView").text("WireGuard").waitFor()
-sleep(6000);
+//home();
+//sleep(1000);
+///launchApp('WireGuard');
+//sleep(6000);
 
 home();
 sleep(1000); 
@@ -51,13 +50,16 @@ let min = now.getMinutes();
 let sec = now.getSeconds();
 let msUntilNextHour = ((60 - min - 1) * 60 * 1000) + (60 - sec) * 1000;
 let ms = ["3700","3800","3900","3950","3750","3850","3900","3950"];
+//let ms = ["4100","4200","4300","4400","4150","4250","4350","4450"];
+//let ms = ["3010","3020","3030","3010","3010","3020","3030","3010"];
 let ms1=Math.floor(Math.random() * 8);
 let msUntilNextHour2= msUntilNextHour  - ms[ms1];
 
 setTimeout(function() {
     click(63,627);
-    sleep(55000);
-    book2();
+    sleep(65000);
+    click(63,627);
+//   book2();
 }, msUntilNextHour2);
 
 
@@ -70,17 +72,16 @@ function book2(){
   // className("android.widget.Button").desc("虛擬等候室").findOne();
   // sleep(1000);
   //  click(63,627);
-    click(63,627);
     sleep(1000);
      className("android.view.View").desc("你的日程表").findOne();
      sleep(1000);
      click(372,1807);
      className("android.view.View").desc("最遠可預約的日期").findOne();
-     sleep(1300);
+     sleep(1800);
          swipe(778,1685,778,1276,1000);
          sleep(500);
          click(778,1452);
-         sleep(1100);
+         sleep(1500);
              if(!(className("android.view.View").desc("檢視並付款").exists())){
                click(702,1163); //  click(530,1165);
              sleep(200);
