212c268a69a42feb4fee46c0c4a16defa8056011
James Ng <70497056+james-ng4321@users.noreply.github.com>
2025-11-30 23:39:03 +0800

    Update ACoppo4.js

--- ACoppo4.js
+++ ACoppo4.js
@@ -28,7 +28,18 @@ sleep(1100);
 click(346,506);
 sleep(1100);
 
+home();
+sleep(1000);
+click('Auto Clicker');
+sleep(4000);
+id("enable_multi_mode_button").findOne().click();
+sleep(4000);
+click(297,781);
+sleep(1000);
 
+home();
+sleep(1100); 
+click('SmartPLAY');
 
 let now = new Date();
 let hour = now.getHours();
