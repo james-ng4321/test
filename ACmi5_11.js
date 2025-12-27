6612e0c1b219a9c87b2b6857403ae4afc01c4a77
James Ng <70497056+james-ng4321@users.noreply.github.com>
2025-12-27 23:14:41 +0800

    Update ACmi5_11.js

--- ACmi5_11.js
+++ ACmi5_11.js
@@ -2,33 +2,32 @@ var a =http.get('https://raw.githubusercontent.com/james-ng4321/test/refs/heads/
 var b=a.body.string();
 var c=b.split(',');
 
-home();
-sleep(1000);
-launchApp('WireGuard');
-//className("android.widget.TextView").text("WireGuard").waitFor()
-sleep(6000);
+//home();
+//sleep(1000);
+///launchApp('WireGuard');
+//sleep(6000);
 
 home();
-sleep(1000); 
+sleep(1100); 
 launchApp('SmartPLAY');
-sleep(6000);
+sleep(8000);
 //className("android.view.View").desc("歡迎來到SmartPLAY").waitFor();
 className("android.widget.Button").desc("登入").findOne().click();
-sleep(8000);
+sleep(11000);
 setClip(c[0]);
-sleep(1000);
+sleep(1500);
 click(502,618);
-sleep(1000);
+sleep(1100);
 click(125,1196);
-sleep(1000);
+sleep(1100);
 setClip(c[1]);
-sleep(1000); 
+sleep(1100); 
 click(504,934);
-sleep(1000);
+sleep(1100);
 click(125,1196);
-sleep(1000);
+sleep(1100);
 click(497,770);
-sleep(1000);
+sleep(1100);
 
 home();
 sleep(1000);
@@ -41,7 +40,7 @@ click(518,1077);
 sleep(1000);
 
 home();
-sleep(1000); 
+sleep(2000); 
 launchApp('SmartPLAY');
 
 sleep(3000); 
@@ -50,14 +49,23 @@ let hour = now.getHours();
 let min = now.getMinutes();
 let sec = now.getSeconds();
 let msUntilNextHour = ((60 - min - 1) * 60 * 1000) + (60 - sec) * 1000;
-let ms = ["3700","3800","3900","3950","3750","3850","3900","3950"];
+//let ms = ["15001","15001","15001","15002","15002","15002","15003","15003"];
+//let ms = ["15001","15001","15001","15002","15002","15002","15003","15003"];
+//let ms = ["4100","4200","4100","4200","4150","4250","4100","4200"];
+//let ms = ["2010","2020","2030","2010","2010","2020","2030","2010"];
+let ms = ["2110","2120","2130","2110","2110","2120","2130","2110"];
+//let ms = ["3002","3002","3001","3001","3001","3002","3003","3003"];
+//let ms = ["5100","5200","5300","5400","5150","5250","5350","5450"];
+//let ms = ["3002","3002","3001","3001","3001","3002","3003","3003"];
+//let ms = ["3000","3000","3001","3001","3001","3000","3000","3001"];
 let ms1=Math.floor(Math.random() * 8);
 let msUntilNextHour2= msUntilNextHour  - ms[ms1];
 
 setTimeout(function() {
     click(63,627);
-    sleep(50000);
-    book2();
+    sleep(65000);
+    click(63,627);
+   //book2();
 }, msUntilNextHour2);
 
 
@@ -70,13 +78,12 @@ function book2(){
   // className("android.widget.Button").desc("虛擬等候室").findOne();
   // sleep(1000);
   //  click(63,627);
-    click(63,627);
     sleep(1000);
      className("android.view.View").desc("你的日程表").findOne();
      sleep(1000);
      click(372,1807);
      className("android.view.View").desc("最遠可預約的日期").findOne();
-     sleep(1500);
+     sleep(1800);
          swipe(778,1685,778,1276,1000);
          sleep(500);
          click(778,1452);
