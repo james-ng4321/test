a4d02d8ed94fe008f92a4f02a6c861c24be195d6
James Ng <70497056+james-ng4321@users.noreply.github.com>
2026-01-31 01:43:45 +0800

    Update ACmi5_2.js

--- ACmi5_2.js
+++ ACmi5_2.js
@@ -61,7 +61,7 @@ let ms1=Math.floor(Math.random() * 8);
 let msUntilNextHour2=30000;
 
 setTimeout(function() {
-    click(63,627);
+  //  click(63,627);
     sleep(20000);
  //   click(63,627);
     book2();
