b0505a6289c949bf5cbed4a324be2e0df26f362d
James Ng <70497056+james-ng4321@users.noreply.github.com>
2026-02-05 01:13:04 +0800

    Update ACmi5_1.js

--- ACmi5_1.js
+++ ACmi5_1.js
@@ -88,7 +88,7 @@ function book2(){
      click(372,1807);
          sleep(200);
      className("android.view.View").desc("最遠可預約的日期").findOne();
-     sleep(1200);
+     sleep(1000);
          swipe(778,1685,778,1276,1000);
          sleep(500);
          click(778,1452);
