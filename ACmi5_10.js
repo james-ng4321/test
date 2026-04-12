d0d86579bc46b3b399e709dfff792b5ba28596d2
James Ng <70497056+james-ng4321@users.noreply.github.com>
2026-04-12 21:34:35 +0800

    Update ACmi5_10.js

--- ACmi5_10.js
+++ ACmi5_10.js
@@ -45,7 +45,10 @@ home();
 sleep(2000); 
 launchApp('SmartPLAY');
 
-sleep(3000); 
+sleep(1500); 
+click(555,1200);
+sleep(1800); 
+click(555,1200);
 let now = new Date();
 let hour = now.getHours();
 let min = now.getMinutes();
