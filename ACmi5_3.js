f7d08bf69fa80c42d8792c07db74e12c7c1f3f17
James Ng <70497056+james-ng4321@users.noreply.github.com>
2026-04-14 00:34:32 +0800

    Update ACmi5_3.js

--- ACmi5_3.js
+++ ACmi5_3.js
@@ -80,7 +80,7 @@ function myScript() {
 }
 
 // 2. Start the interval (e.g., every 1000ms = 1s)
-const intervalId = setInterval(myScript, 10);
+const intervalId = setInterval(myScript, 50);
 
 // 3. Stop the interval after 8 seconds (8000ms)
 setTimeout(() => {
