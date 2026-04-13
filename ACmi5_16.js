430335b544ff5f3e7087c6c5fdadcd23715d7f62
James Ng <70497056+james-ng4321@users.noreply.github.com>
2026-04-13 12:03:52 +0800

    Update ACmi5_16.js

--- ACmi5_16.js
+++ ACmi5_16.js
@@ -45,10 +45,6 @@ home();
 sleep(1000); 
 click(150,1360);
 
-sleep(8100);
-click(555,1200);
-sleep(4100); 
-click(555,1200);
 
 let now = new Date();
 let hour = now.getHours();
@@ -77,8 +73,12 @@ setTimeout(function() {
 }, msUntilNextHour2);
 
 setTimeout(function() {
+click(555,1200);
+}, msUntilNextHour2 - 10000);
+
+setTimeout(function() {
   click(250,1155);
-}, msUntilNextHour);
+}, msUntilNextHour - 500);
 
 function book2(){
   // sleep(1000);
