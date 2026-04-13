b68db0e2a16ffc24bed37573c25216a507444bb6
James Ng <70497056+james-ng4321@users.noreply.github.com>
2026-04-13 11:59:52 +0800

    Update ACmi5_8a.js

--- ACmi5_8a.js
+++ ACmi5_8a.js
@@ -45,10 +45,6 @@ home();
 sleep(1100); 
 launchApp('SmartPLAY');
 
-sleep(8100);
-click(555,1200);
-sleep(4100); 
-click(555,1200);
 
 let now = new Date();
 let hour = now.getHours();
@@ -75,8 +71,12 @@ setTimeout(function() {
 }, msUntilNextHour2);
 
 setTimeout(function() {
+click(555,1200);
+}, msUntilNextHour2 - 10000);
+
+setTimeout(function() {
   click(250,1155);
-}, msUntilNextHour);
+}, msUntilNextHour - 500);
 
 function book2(){
   // sleep(1000);
