430f1e53efa890cff2273281b4534954e7929cc5
James Ng <70497056+james-ng4321@users.noreply.github.com>
2026-05-04 17:35:57 +0800

    Update ACmi5_6.js

--- ACmi5_6.js
+++ ACmi5_6.js
@@ -79,7 +79,7 @@ setTimeout(function() {
 click(555,1200);
  sleep(2100); 
 click(1050,1400);
-}, msUntilNextHour2 - 304000);
+}, msUntilNextHour2 - 424000);
 
 setTimeout(function() {
   click(250,1155);
