ab7240746870180c0c0d7a4302ce26fe031fd4ea
James Ng <70497056+james-ng4321@users.noreply.github.com>
2026-05-17 23:04:00 +0800

    Update ACmi5_6a.js

--- ACmi5_6a.js
+++ ACmi5_6a.js
@@ -8,9 +8,9 @@ var c=b.split(',');
 //sleep(6000);
 
 home();
-sleep(1000); 
+sleep(2000); 
   click(150,1360);
-sleep(6000);
+sleep(6100);
 //className("android.view.View").desc("歡迎來到SmartPLAY").waitFor();
 className("android.widget.Button").desc("登入").findOne().click();
 sleep(8000);
@@ -44,6 +44,7 @@ sleep(1000);
 home();
 sleep(1000); 
 click(150,1360);
+
 
 //sleep(8100);
 //click(555,1200);
