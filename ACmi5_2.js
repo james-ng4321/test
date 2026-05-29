050bf67249d620abdac57b93a6b1d4b988815945
James Ng <70497056+james-ng4321@users.noreply.github.com>
2026-05-29 10:15:53 +0800

    Update ACmi5_2.js

--- ACmi5_2.js
+++ ACmi5_2.js
@@ -74,7 +74,7 @@ let msUntilNextHour2= msUntilNextHour  - ms[ms1];
 setTimeout(function() {
  // click(500,1200);
      click(63,627);
-    sleep(9000);
+    sleep(6300);
      click(63,627);
     sleep(200);
     book2();
