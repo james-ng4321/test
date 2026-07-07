86913c50fd6ae81b9b656efde608b00afe6ac775
James Ng <70497056+james-ng4321@users.noreply.github.com>
2026-07-08 07:33:03 +0800

    Update NewMi5_4.js

--- NewMi5_4.js
+++ NewMi5_4.js
@@ -13,11 +13,11 @@ sleep(1000);
 home();
 sleep(2000); 
 app.launchApp("SmartPLAY");
-sleep(5000); 
-className("android.widget.Button").desc("登入").findOne().click();
-sleep(5000);
-click(380,1750);
-//sleep(20000);
+//sleep(5000); 
+//className("android.widget.Button").desc("登入").findOne().click();
+//sleep(5000);
+//click(380,1750);
+//sleep(10000);
 //click(550,1700);
 
 let now = new Date();
