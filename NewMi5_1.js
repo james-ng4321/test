c18787d151c81ed9d079501d393e48a28a50b13f
James Ng <70497056+james-ng4321@users.noreply.github.com>
2026-07-07 15:04:44 +0800

    Create NewMi5_1.js

---
+++ NewMi5_1.js
@@ -0,0 +1,100 @@
+home();
+sleep(1000);
+launchApp('Auto Clicker');
+sleep(4000);
+//className("android.widget.TextView").text("Auto Clicker 🎯 (Free)").waitFor();
+id("enable_multi_mode_button").findOne().click();
+sleep(2000);
+click(518,1077);
+sleep(1000);
+click(76,1287);
+sleep(1000);
+
+home();
+sleep(2000); 
+app.launchApp("SmartPLAY");
+sleep(5000); 
+className("android.widget.Button").desc("登入").findOne().click();
+sleep(5000);
+click(380,1750);
+//sleep(20000);
+//click(550,1700);
+
+let now = new Date();
+let hour = now.getHours();
+let min = now.getMinutes();
+let sec = now.getSeconds();
+let msUntilNextHour = ((60 - min - 1) * 60 * 1000) + (60 - sec) * 1000;
+//let ms = ["3910","3920","3930","3910","3910","3920","3930","3910"];
+//let ms = ["2510","2520","2530","2510","2510","2500","2530","2510"];
+//let ms = ["1010","1020","1030","1010","1010","1020","1030","1010"];
+let ms = ["2910","2920","2930","2910","2910","2920","2930","2910"];
+//let ms = ["1910","1920","1930","1910","1910","1920","1930","1910"];
+//let ms = ["2034","2045","2076","2078","2012","2079","2034","2012"];
+//let ms = ["4100","4200","4300","4400","4150","4250","4350","4450"];
+//let ms = ["6100","6200","6300","6400","6150","6250","6350","6450"];
+let ms1=Math.floor(Math.random() * 8);
+let msUntilNextHour2= msUntilNextHour  - ms[ms1];
+
+setTimeout(function() {
+ 
+    click(63,627);
+    sleep(6800);
+    click(63,627);
+    book2();
+}, msUntilNextHour2);
+
+
+
+function book2(){
+  // sleep(1000);
+  // className("android.widget.Button").desc("虛擬等候室").findOne();
+  // sleep(1000);
+  //  click(63,627);
+   //  sleep(1200);
+     className("android.view.View").desc("你的日程表").findOne();
+    sleep(200);
+     //sleep(1000);
+     click(372,1807);
+         sleep(200);
+    // className("android.view.View").desc("最遠可預約的日期").findOne();
+    //71
+      className("android.view.View").desc("再次租訂").findOne();
+       //71
+     sleep(1500);
+        swipe(778,1685,778,1550,500);
+       //71
+  // swipe(830,1694,830,1220,500);
+       //71
+         sleep(200);
+         click(778,1652);
+         sleep(1200);
+             if(!(className("android.view.View").desc("檢視並付款").exists())){
+               click(702,1163); //  click(530,1165);
+             sleep(200);
+             if((!(className("android.view.View").desc("注視列表").exists()))&&(!(className("android.view.View").desc("檢視並付款").exists()))){
+               sleep(1000);
+               click(702,1163); //  click(530,1165);
+               sleep(200);  //} else { sleep(1000);
+             if((!(className("android.view.View").desc("注視列表").exists()))&&(!(className("android.view.View").desc("檢視並付款").exists()))){
+               sleep(1000);
+               click(702,1163); //  click(530,1165);
+               sleep(200);}}  
+           swipe(885,1365,52,1385,1100);
+         sleep(200);
+           click(665,1658);
+           sleep(1100);
+             if(!(className("android.view.View").desc("檢視並付款").exists())){
+               click(702,1163); //  click(530,1165);
+             sleep(200);
+             if((!(className("android.view.View").desc("注視列表").exists()))&&(!(className("android.view.View").desc("檢視並付款").exists()))){
+               sleep(1100);
+               click(702,1163); //  click(530,1165);
+               sleep(200);//} else { sleep(1000);
+             if((!(className("android.view.View").desc("注視列表").exists()))&&(!(className("android.view.View").desc("檢視並付款").exists()))){
+               sleep(1000);
+               click(702,1163); //  click(530,1165);
+               sleep(200);}}      
+           click(967,1658);
+         }}
+  } 
